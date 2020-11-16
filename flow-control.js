9b82eb795b3e627f2bfe6d478880ca50ceb460ee
Anke <anke.ortmann@gmail.com>
2020-11-16 14:49:14 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,11 @@
-function basicTeenager(age) {
-
+function basicTeenager(13-19) {
+"You are a teenager!"
 }
 
-function teenager(age) {
-
+function teenager(13-19) {
+"You are a teenager!"
+} else {
+  "You are not a teenager"
 }
 
 function ageChecker(age) {
